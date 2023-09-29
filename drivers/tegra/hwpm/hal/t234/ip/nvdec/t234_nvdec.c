f510c8652829c625b85cb19cdd51db72b1271f58
vasukis <vasukis@nvidia.com>
2023-09-29 20:44:31 +0000

    tegra: hwpm: Add NVDEC IP debug node info
    HWPM resource manager in QNX will query register read/write
    ops to the IP debug node exposed. This is done via devctl
    calls from the HWPM Res Mgr. Hence, update the NVDEC debug
    node name in ip source file.
    
    Bug 4170733
    DOS-SHR-7601
    
    Change-Id: I817aa18be43534907d761c992b9953918a39525d
    Signed-off-by: vasukis <vasukis@nvidia.com>
    (cherry picked from commit 7ed60c287e1253b834bfe050952240e97549e320)
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-hwpm/+/2991341
    Reviewed-by: Vishal Aslot <vaslot@nvidia.com>
    Reviewed-by: Seema Khowala <seemaj@nvidia.com>
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>

--- drivers/tegra/hwpm/hal/t234/ip/nvdec/t234_nvdec.c
+++ drivers/tegra/hwpm/hal/t234/ip/nvdec/t234_nvdec.c
@@ -127,11 +127,11 @@ static struct hwpm_ip_inst t234_nvdec_inst_static_array[
 			.ip_dev = NULL,
 			.hwpm_ip_pm = NULL,
 			.hwpm_ip_reg_op = NULL,
-			.fd = TEGRA_HWPM_IP_DEBUG_FD_INVALID,
+			.fd = TEGRA_HWPM_IP_DEBUG_FD_VALID,
 		},
 
 		.element_fs_mask = 0U,
-		.dev_name = "",
+		.dev_name = "/dev/nvhost-debug/nvdec_hwpm",
 	},
 };
 
